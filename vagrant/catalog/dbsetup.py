347097e5dac3e79ba4f53be767ba287e4673935f
Dylan <dylan@Dylans-MacBook-Pro-2.local>
2018-10-15 09:50:32 -0700

    virtual machine ready

--- vagrant/catalog/dbsetup.py
+++ vagrant/catalog/dbsetup.py
@@ -5,12 +5,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import func
 
-class Goal(Base):
+class Student(Base):
     __tablename__ = 'goal'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    goal_name = Column(String(32))
-    goal_description=Column(String(32))
+    student_name = Column(String(32))
+    student_class = Column(String(32))
+    student_email = Column(String(32))
 
 engine = create_engine('sqlite:///database.db')
 Base.metadata.create_all(engine)

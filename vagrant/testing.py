7367193908b18a893238962e94ecfd4e6b80d6ea
unknown <frankatank1@gmail.com>
2018-10-24 14:42:52 -0700

    .

--- vagrant/testing.py
+++ vagrant/testing.py
@@ -3,20 +3,21 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, StudentCreator, Student
 from teacherActions import makeStudent
-from teacherActions import getStudent
+from teacherActions import getStudent, session, app
 import unittest
 import os
 
 app = Flask(__name__)
-
-engine = create_engine('sqlite:///:memory:')
+    
+engine = create_engine('sqlite:///testing.db')
 
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 dal = Student()
-TEST_DB = 'test.db'
+
+TEST_DB = 'testing.db'
 class TestApp(unittest.TestCase):
     # executed prior to each test
     def setUp(self):
@@ -26,33 +27,21 @@ class TestApp(unittest.TestCase):
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
             os.path.join(app.config['testing.db'], TEST_DB)
         self.app = app.test_client()
-        db.drop_all()
-        db.create_all()
-
-        # Disable sending emails during unit testing
-        mail.init_app(app)
-        self.assertEqual(app.debug, False)
-
-
-
+        session.drop_all()
+        session.create_all()
     # executed after each test
     def tearDown(self):
         pass
-###############
-#### tests ####
-###############
-
-
-    def test_CreatingStudent(self):
-        testStudent = makeStudent("test name", "yeet on me")
-        grab = getStudent("yeet on me")
-        self.assertEqual(testStudent.name, grab.goal)
 
 
     def test_main_page(self):
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
+    def test_CreatingStudent(self):
+        testStudent = makeStudent("test name", "yeet on me")
+        grab = getStudent("yeet on me")
+        self.assertEqual(testStudent.name, grab.goal)
+
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0', port=8000)
+    unittest.main()

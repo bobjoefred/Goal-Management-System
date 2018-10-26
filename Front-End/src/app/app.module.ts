67b0826a162ded4771da45ad950f7368840f73b4
Olivea Varady <ovarady2019@chadwickschool.org>
2018-10-26 11:51:03 -0700

    home page and footer component created

--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule

2cadcb7f49e9f731e2ff54d13859cb242269fc40
Dylan <dnguyen2020@chadwickschool.org>
2018-12-02 09:06:20 -0800

    Add: Created a database populator to test the functionality of the database and login functions

--- vagrant/catalog/database-populator.py
+++ vagrant/catalog/database-populator.py
@@ -10,4 +10,17 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 student1 = Student(name = "joe@joe.com" )
+session.add(student1)
+session.commit(student1)
 
+student2 = Student(name = "dylan888guy@gmail.com")
+session.add(student1)
+session.commit(student1)
+
+teacher1 = Teacher(name = "devoon@devoonboon.com")
+session.add(teacher1)
+session.commit(teacher1)
+
+teacher2 = Teacher(name = "dnguyen2020@chadwickschool.org")
+session.add(teacher2)
+session.commit(teacher2)

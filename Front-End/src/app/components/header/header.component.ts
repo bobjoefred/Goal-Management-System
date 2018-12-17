1e738b47205bd0537074067a590145f6f3b6d9c2
Olivea Varady <ovarady2019@chadwickschool.org>
2018-12-17 09:45:20 -0800

    style: update header page

--- Front-End/src/app/components/header/header.component.ts
+++ Front-End/src/app/components/header/header.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  isTeacher: boolean = true;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  // isTeacher() {
+  //   // TODO
+  //   return false;
+  // }
 
 }

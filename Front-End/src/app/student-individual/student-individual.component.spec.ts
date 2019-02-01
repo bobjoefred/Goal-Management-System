5482832b0501f2874280b6f778b8804393fce36d
Olivea Varady <ovarady2019@chadwickschool.org>
2019-02-01 12:24:41 -0800

    style: add format to student history and student individual component

---
+++ Front-End/src/app/student-individual/student-individual.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StudentIndividualComponent } from './student-individual.component';
+
+describe('StudentIndividualComponent', () => {
+  let component: StudentIndividualComponent;
+  let fixture: ComponentFixture<StudentIndividualComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ StudentIndividualComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StudentIndividualComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Front-End/src/app/student-individual/student-individual.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-individual',
+  templateUrl: './student-individual.component.html',
+  styleUrls: ['./student-individual.component.css']
+})
+export class StudentIndividualComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

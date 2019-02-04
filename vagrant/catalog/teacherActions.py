eb45ad4c499f217705cb87d975031b7b7cf4b7e9
unknown <frankatank1@gmail.com>
2019-02-04 09:57:56 -0800

    Chore: removed unnecessary lines

--- vagrant/catalog/teacherActions.py
+++ vagrant/catalog/teacherActions.py
@@ -2,19 +2,12 @@ import flask
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Teacher, Student, Goal
+from database_setup import Base, Teacher, Student, Goal, StudentGoalLink
 from sqlalchemy import DateTime
 
 
 
-app = Flask(__name__)
 
-engine = create_engine('sqlite:///teacheractions.db')
-
-Base.metadata.bind = engine
-
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
 
 def makeStudent(name, sesh):
     if(name == None):
@@ -25,7 +18,7 @@ def makeStudent(name, sesh):
     sesh.add(student)
     sesh.commit()
     return student
-    '''return "TODO: Implement", 200'''
+        '''return "TODO: Implement", 200'''
 #creating a goal and assigning seperate
 #to assign a goal, create a new goal
 #def createGoal():
@@ -34,7 +27,7 @@ def createTeacher(name, login, password ,session):
     session.add(teacher)
     session.commit()
 
-    return teacher
+        return teacher
 def showStudents(session):
     students = session.query(Student).all()
     studentList = []
@@ -59,6 +52,18 @@ def showGoals(session):
 
     #print(studentList)
     return flask.jsonify(goalList)
+def showStudentGoals(student, session):
+    goals = student.goals
+    goalList = []
+    #look in itemcatalog to see how the project deals with serialized objects
+    for goal in goals:
+
+        goalList.append(goal.serialize)
+    #    studentList += jsonify(Student=student.serialize)
+    #    studentList += thisStudent
+
+    #print(studentList)
+    return flask.jsonify(goalList)
 def createGoal(name, description, dueDate, session):
     if(name == None or description == None):
         return "Missing name or description, 404"
@@ -71,16 +76,11 @@ def createGoal(name, description, dueDate, session):
     session.commit()
     return goal
 def assignTeacher(teacher, goal, session):
-    #self suggestion: maybe make it so all of them query session instead of directly being passed in?
     goal.createdBy = teacher.id
     session.add(goal)
     session.commit()
 
 def assignGoal(student, goal, session):
-    student.goals.append(goal)
-    session.add(student)
+    student_goal_link = StudentGoalLink(student_id = student.id, goal_id = goal.id, isCompleted = False)
+    session.add(student_goal_link)
     session.commit()
-
-if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0', port=8000)

37db31b12af49eeea40dc9ad7ae17a336a5dc7be
Olivea Varady <ovarady2019@chadwickschool.org>
2019-02-11 10:19:21 -0800

    feat: add student reject component

---
+++ Front-End/src/app/student-reject/student-reject.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StudentRejectComponent } from './student-reject.component';
+
+describe('StudentRejectComponent', () => {
+  let component: StudentRejectComponent;
+  let fixture: ComponentFixture<StudentRejectComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ StudentRejectComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StudentRejectComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Front-End/src/app/student-reject/student-reject.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-reject',
+  templateUrl: './student-reject.component.html',
+  styleUrls: ['./student-reject.component.css']
+})
+export class StudentRejectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
